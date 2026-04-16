54e61315e329dc9e50b29074d2263a49c4d06276
kiennd <ndkien98>
2026-04-16 21:00:26 +0700

    finish ui

---
+++ 6. Spring boot/buoi20/uniqlo-service-v1/uniqlo-service-v1/src/main/java/com/t3h/uniqlo/controller/CmsController.java
@@ -0,0 +1,41 @@
+package com.t3h.uniqlo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/cms")
+public class CmsController {
+
+    @GetMapping
+    public String dashboard() {
+        return "cms_dashboard";
+    }
+
+    @GetMapping("/categories")
+    public String categories() {
+        return "cms_categories";
+    }
+
+    @GetMapping("/products")
+    public String products() {
+        return "cms_products";
+    }
+
+    @GetMapping("/skus")
+    public String skus() {
+        return "cms_skus";
+    }
+
+    @GetMapping("/reviews")
+    public String reviews() {
+        return "cms_reviews";
+    }
+
+    @GetMapping("/visit-stats")
+    public String visitStats() {
+        return "cms_visit_stats";
+    }
+}
+
--- 6. Spring boot/buoi20/uniqlo-service-v1/uniqlo-service-v1/src/main/java/com/t3h/uniqlo/controller/UserController.java
+++ 6. Spring boot/buoi20/uniqlo-service-v1/uniqlo-service-v1/src/main/java/com/t3h/uniqlo/controller/UserController.java
@@ -3,14 +3,14 @@ package com.t3h.uniqlo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
     @GetMapping
     public String getUsers() {
-        return "user-list";
+        // maps to src/main/resources/templates/user_list.html
+        return "user_list";
     }
 
 }
